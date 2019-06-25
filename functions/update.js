7612b094d640987e9d2e6cb70409417310329d5e
Joey Kaan <jkaan@users.noreply.github.com>
2019-06-25 21:24:48 +0200

    Use async function

--- functions/update.js
+++ functions/update.js
@@ -1,6 +1,6 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
+exports.handler = async (event, context) => {
+  return {
     statusCode: 200,
     body: 'No worries, all is working fine!'
-  })
-}
+  };
+};
