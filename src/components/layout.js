18580ca11fcb073421db7045191646f6f3bff760
Joey Kaan <jkaan@users.noreply.github.com>
2019-11-20 19:53:35 +0100

    Remove meta tags we dont need

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,11 +26,6 @@ const Layout = ({
     <Helmet>
       <link rel="shortcut icon" type="image/x-icon" href={favicon} />
       <meta name="2fdeccbcacfeee9" content="f0069a37446db760e09735e27c0e157c" />
-      <meta
-        name="trade-tracker-meta-key"
-        content="Verificatie Daan Marechal als eigenaar voor Tradetracker"
-      />
-      <meta name="verification" content="760a132b19b9d8536622b510f4a5892c" />
     </Helmet>
     <ChildrenContainer isIndex={isIndex}>{children}</ChildrenContainer>
   </>
